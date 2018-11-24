6199951d4e009f6ca519f7d1275862aa9c2dfc99
Maxwell Simmer <maxwell.simmer@gmail.com>
2018-11-24 21:36:32 +0100

    Adds demo for markdown-it-container

--- markdown-it-plugin.config.js
+++ markdown-it-plugin.config.js
@@ -2,6 +2,20 @@ const d3node = require('d3-node')
 
 module.exports = {
     plugins: [
+        ['markdown-it-container', {
+            args: [
+              'spoiler',
+              {
+                validate: params => params.trim().match(/^spoiler(.*)$/),
+                render: (tokens, idx) => {
+                  const match = tokens[idx].info.trim().match(/^spoiler(.*)$/)
+                  return tokens[idx].nesting === 1
+                    ? `<details><summary>Spoiler</summary>${match[1]}`
+                    : '</details>'
+                }
+              }
+          ],
+        }],
         'markdown-it-emoji',
         'markdown-it-mathjax',
         ['markdown-it-for-inline', {
---
+++ tests/plugins-container.md
@@ -0,0 +1,30 @@
+# Plugin Test
+
+This uses the [`markdown-it-container`](https://github.com/markdown-it/markdown-it-container) package.
+
+## Config
+
+```js
+module.exports = {
+  ['markdown-it-container', {
+      args: [
+        'spoiler',
+        {
+          validate: params => params.trim().match(/^spoiler(.*)$/),
+          render: (tokens, idx) => {
+            const match = tokens[idx].info.trim().match(/^spoiler(.*)$/)
+            return tokens[idx].nesting === 1
+              ? `<details><summary>Spoiler</summary>${match[1]}`
+              : '</details>'
+          }
+        }
+    ],
+  }],
+}
+```
+
+## Result
+
+::: spoiler
+*here be dragons*
+:::
